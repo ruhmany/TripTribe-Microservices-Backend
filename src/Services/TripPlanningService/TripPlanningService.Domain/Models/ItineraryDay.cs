6e4d71050667657f72df40e2f37b1c2b06811b1b
abdelrahman.elnaggar <abdelrahman.elnaggar@sewedy.com.eg>
2026-01-23 22:44:08 +0200

    Implement the aggregate and the entities of this serrvice

---
+++ src/Services/TripPlanningService/TripPlanningService.Domain/Models/ItineraryDay.cs
@@ -0,0 +1,11 @@
+﻿namespace TripPlanningService.Domain.Models
+{
+    public class ItineraryDay : Entity<ItineraryDayId>
+    {
+        public Guid Id { get; private set; }
+        public DateOnly Date { get; private set; }
+
+        private readonly List<Activity> _activities = new();
+        public IReadOnlyCollection<Activity> Activities => _activities;
+    }
+}
---
+++ src/Services/TripPlanningService/TripPlanningService.Domain/Models/TripCollaborator.cs
@@ -0,0 +1,8 @@
+﻿namespace TripPlanningService.Domain.Models
+{
+    public class TripCollaborator
+    {
+        public Guid UserId { get; private set; }
+        public TripRole Role { get; private set; }
+    }
+}
